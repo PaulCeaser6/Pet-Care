240d6e46e149c069ffaadebec45960df73800f48
PaulCeaser6 <paulceaser3@gmail.com>
2022-12-28 23:06:06 -0800

    Just made an important change

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const toggleBar = document.querySelector('#toggle');
 const closeBar = document.querySelector('#close');
 const sideMenu = document.querySelector('#sidebar');
 
+// just trying out new things
+
 toggleBar.addEventListener("click", function() {
    sideMenu.style.width = "250px";
 })
