375f19b85ec79147275580253c636348dc322af5
PaulCeaser6 <paulceaser3@gmail.com>
2022-12-28 23:09:49 -0800

    I removed the comment

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const toggleBar = document.querySelector('#toggle');
 const closeBar = document.querySelector('#close');
 const sideMenu = document.querySelector('#sidebar');
 
-// just trying out new things
 
 toggleBar.addEventListener("click", function() {
    sideMenu.style.width = "250px";
